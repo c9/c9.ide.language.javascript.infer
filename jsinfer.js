7e730e3ae099cbd22b5fc5bee241e0137a04b8ff
Ruben Daniels <ruben@c9.io>
2014-01-27 12:19:46 +0100

    Big rename from 'Cloud9 IDE' to 'Cloud9'

--- jsinfer.js
+++ jsinfer.js
@@ -1,5 +1,5 @@
 /**
- * Inference-based code completion for the Cloud9 IDE
+ * Inference-based code completion for the Cloud9
  *
  * @copyright 2010, Ajax.org B.V.
  * @license GPLv3 <http://www.gnu.org/licenses/gpl.txt>
